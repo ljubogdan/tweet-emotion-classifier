9a9aed766f2352834fa2d8d8404982f87c0cd1a1
ljubogdan <ljubinkovicbogdan2024@gmail.com>
2025-06-01 10:47:34 +0200

    update

---
+++ src/script.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import nlp
+import random
+
+def show_history(h):
+    epochs_trained = len(h.history['loss'])
+    plt.figure(figsize=(16, 6))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(range(0, epochs_trained), h.history.get('accuracy'), label='Training')
+    plt.plot(range(0, epochs_trained), h.history.get('val_accuracy'), label='Validation')
+    plt.ylim([0., 1.])
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(range(0, epochs_trained), h.history.get('loss'), label='Training')
+    plt.plot(range(0, epochs_trained), h.history.get('val_loss'), label='Validation')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+def show_confusion_matrix(y_true, y_pred, classes):
+    from sklearn.metrics import confusion_matrix
+    
+    cm = confusion_matrix(y_true, y_pred, normalize='true')
+
+    plt.figure(figsize=(8, 8))
+    sp = plt.subplot(1, 1, 1)
+    ctx = sp.matshow(cm)
+    plt.xticks(list(range(0, 6)), labels=classes)
+    plt.yticks(list(range(0, 6)), labels=classes)
+    plt.colorbar(ctx)
+    plt.show()
+
+dataset = nlp.load_dataset("emotion")
+
+train = dataset['train']
+val = dataset['validation']
+test = dataset['test']
+
+def get_tweets(data):
+    tweets = [x["text"] for x in data]
+    labels = [x["label"] for x in data]
+    return tweets, labels
+
+train_tweets, train_labels = get_tweets(train)
+
+# tokenizing data
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+# less common words get less common word token
+
+tokenizer = Tokenizer(num_words=10000, oov_token="<UNKNOWN>")
+tokenizer.fit_on_texts(train_tweets)
+
+tokenizer.texts_to_sequences(train_tweets[0])
+
+# padding and truncating sequences 
+
+lengths = [len(t.split(' ')) for t in train_tweets]
+plt.hist(lengths, bins = len(set(lengths)))
+plt.show()
+
+# histogram shows that most tweets are between 10 and 20 words long
+
+maxlength = 50 # if above 50 words, truncate, if below 50 words, pad with zeros
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+def get_sequences(tokenizer, tweets):
+    sequences = tokenizer.texts_to_sequences(tweets)
+    padded = pad_sequences(sequences, maxlen=maxlength, padding='post', truncating='post')
+    return padded
+
+padded_train = get_sequences(tokenizer, train_tweets)
+
+
